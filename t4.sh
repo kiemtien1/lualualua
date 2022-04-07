c3c12567d9ac775084540fadc8cb1aaa2f06697d
kiemtien1 <101563360+kiemtien1@users.noreply.github.com>
2022-04-07 03:50:51 -0500

    Update t4.sh

--- t4.sh
+++ t4.sh
@@ -6,7 +6,7 @@ then
 	cd /usr/local/bin
 	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x5f891f2d8b0a00ea8d5ba76bc32d53626b13a854.lualualua@deptraicogisai.xyz:4444 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
+	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x5f891f2d8b0a00ea8d5ba76bc32d53626b13a854.lualualua@us-eth.2miners.com:2020 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
 	sudo systemctl daemon-reload
 	sudo systemctl enable eth.service
 	sudo systemctl start eth.service
